5d2cf97047eadef74184dc094b916464ccb17f77
Ja0nz <mail@ja.nz>
2019-12-19 22:38:27 +0100

    Adapted gpg

--- gpg/install.sh
+++ gpg/install.sh
@@ -6,8 +6,16 @@ source $(readlink -f "$0" | xargs dirname)/../helpers.sh
 DECRYPT $SOURCE
 
 cd $SOURCE
-awk '!/^#/' ownertrust.txt | while read line; do
-        KEY=$(echo $line | cut -d: -f1 | tail -c 17)
-	test -f $KEY.secret.key && gpg --import $KEY.secret.key
+for file in *.sec.key; do
+	gpg --import $file
+done
+for file in *.pub.key; do
+	gpg --import $file
 done
 gpg --import-ownertrust ownertrust.txt
+
+# Previously
+#awk '!/^#/' ownertrust.txt | while read line; do
+#        KEY=$(echo $line | cut -d: -f1 | tail -c 17)
+#	test -f $KEY.secret.key && gpg --import $KEY.secret.key
+#done
--- gpg/tar.sh
+++ gpg/tar.sh
@@ -5,8 +5,10 @@ source $(readlink -f "$0" | xargs dirname)/../helpers.sh
 
 cd $SOURCE
 gpg --list-secret-keys --with-colons 2> /dev/null | grep '^sec:' | cut -d: -f5 | while read line; do
-	gpg -a --export $line > $line.public.key
-	gpg -a --export-secret-keys $line > $line.secret.key
+	gpg -a --export-secret-keys $line > $line.sec.key
+done
+gpg --list-keys --with-colons 2> /dev/null | grep '^pub:' | cut -d: -f5 | while read line; do
+	gpg -a --export $line > $line.pub.key
 done
 gpg --export-ownertrust > ownertrust.txt
 cd ..
