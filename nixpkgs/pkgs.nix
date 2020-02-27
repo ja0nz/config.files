928e2ba02eb2ae401f9254c8a0151838fa76bc14
Ja0nz <mail@ja.nz>
2020-02-27 21:11:44 +0100

    Added nim; Moved config to home manager

---
+++ nixpkgs/pkgs.nix
@@ -0,0 +1,25 @@
+[
+    "firefox"
+    "htop"
+    "emacs"
+    "neovim"
+    "ripgrep"
+    "fzf"
+    "bat"
+    "okular"
+    "unp"
+    "redshift"
+    "mu"
+    "isync"
+    "pandoc"
+    "rlwrap"
+    "any-nix-shell"
+    # Build stuff
+    "direnv"
+    "graphviz"
+    # Nim -> using the latest
+    "unstable.nim"
+    # To delete if managed in home-manager
+    "kitty"
+    "fish"
+  ]
